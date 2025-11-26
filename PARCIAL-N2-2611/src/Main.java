320539af0fa8a150bf0666b8f30b2b8ba33d17eb
Niquinhoo <nicotrib699@gmail.com>
2025-11-26 19:51:15 -0300

    cambio de App a main

--- PARCIAL-N2-2611/src/Main.java
+++ PARCIAL-N2-2611/src/Main.java
@@ -1,7 +1,6 @@
 import estandar.ListaEnlazada;
 
-
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("\n--- Sistema de Gestion de Biblioteca ---");
 
